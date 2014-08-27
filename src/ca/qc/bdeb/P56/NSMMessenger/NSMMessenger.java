49aed7beed7ca91538c04a5f8e578c828ee06ebb
1150580 <1150580@10.159.17.149>
2014-08-27 09:20:10 -0400

    hola

--- src/ca/qc/bdeb/P56/NSMMessenger/NSMMessenger.java
+++ src/ca/qc/bdeb/P56/NSMMessenger/NSMMessenger.java
@@ -16,7 +16,7 @@ public class NSMMessenger {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Dufour");
+        System.out.println("Dufour26");
     }
     
 }
