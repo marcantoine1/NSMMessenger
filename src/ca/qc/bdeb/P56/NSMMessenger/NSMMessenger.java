763a3bf16c233435af7730b5ea0d07fb2c3e9692
1150275 <1150275@10.159.17.100>
2014-08-27 08:54:17 -0400

    Premier commit. Création des package

---
+++ src/ca/qc/bdeb/P56/NSMMessenger/NSMMessenger.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.qc.bdeb.P56.NSMMessenger;
+
+/**
+ *
+ * @author 1150275
+ */
+public class NSMMessenger {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
