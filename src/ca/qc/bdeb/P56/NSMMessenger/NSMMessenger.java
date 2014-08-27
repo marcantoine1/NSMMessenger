ec7a2ee8b2b48d25739a9ec54bb52d846cf0d1f7
Guillaume <Guillaume@10.150.134.48>
2014-08-27 10:09:35 -0400

    Bye

--- src/ca/qc/bdeb/P56/NSMMessenger/NSMMessenger.java
+++ src/ca/qc/bdeb/P56/NSMMessenger/NSMMessenger.java
@@ -18,7 +18,7 @@ public class NSMMessenger {
     public static void main(String[] args) {
         System.out.println("Dufour26");
         System.out.println("Grégoire");
-        System.out.println("Hello");
+        System.out.println("Bye");
         System.out.println("Boudreau");
         System.out.println("Dube");
         //TODO Test des todo
