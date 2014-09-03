3048f1208e12e42f90586717afae59df9ff74d49
1150580 <1150580@10.159.17.100>
2014-09-03 09:19:20 -0400

    Début du serveur

--- src/ca/qc/bdeb/P56/NSMMessengerServer/NSMServer.java
+++ src/ca/qc/bdeb/P56/NSMMessengerServer/NSMServer.java
@@ -6,7 +6,12 @@
 
 package ca.qc.bdeb.P56.NSMMessengerServer;
 
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -14,7 +19,24 @@ import com.esotericsoftware.kryonet.Server;
  */
 public class NSMServer {
     public static void main(String[] args) {
+        
         System.out.println("Je suis un serveur");
         Server server = new Server();
+        server.start();
+        try {
+            server.bind(54123);
+        } catch (IOException ex) {
+            Logger.getLogger(NSMServer.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Erreur: socket ne peut pas être bind");
+            System.exit(1);
+        }
+        
+        server.addListener(new Listener(){
+            @Override
+            public void received (Connection connection, Object object)
+            {
+                
+            }
+        });
     }
 }
