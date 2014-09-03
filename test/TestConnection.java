7ce5a44c6d52efc0795da180660e2b1001e26d7d
1150580 <1150580@10.159.17.100>
2014-09-03 09:34:27 -0400

    début fichier test connection

--- test/TestConnection.java
+++ test/TestConnection.java
@@ -8,6 +8,41 @@
  *
  * @author 1150580
  */
+
+    import org.junit.After;
+    import org.junit.AfterClass;
+    import org.junit.Before;
+    import org.junit.BeforeClass;
+    import org.junit.Test;
+
 public class TestConnection {
+    public TestConnection(){
+    }
+    
+    @BeforeClass
+    public static void setUpClass() 
+    {
+    }
+    
+     @AfterClass
+    public static void tearDownClass() 
+    {
+    }
+    
+    @Before
+    public void setUp() 
+    {
+    }
+    
+    @After
+    public void tearDown() 
+    {
+    }
+    
+    @Test
+    public void testConnection()
+    {
+        
+    }
     
 }
