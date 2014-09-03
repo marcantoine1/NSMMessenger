174ba7d92afdda184935d3f55a0cdd8480687cb7
1150580 <1150580@10.159.17.100>
2014-09-03 09:27:43 -0400

    objet loginrequest

--- src/ca/qc/bdeb/P56/NSMMessengerServer/NSMServer.java
+++ src/ca/qc/bdeb/P56/NSMMessengerServer/NSMServer.java
@@ -6,6 +6,8 @@
 
 package ca.qc.bdeb.P56.NSMMessengerServer;
 
+import ca.qc.bdeb.P56.NSMMessengerCommunication.LoginRequest;
+import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
@@ -21,7 +23,12 @@ public class NSMServer {
     public static void main(String[] args) {
         
         System.out.println("Je suis un serveur");
+        
         Server server = new Server();
+        
+        Kryo kryo = server.getKryo();
+        kryo.register(LoginRequest.class);
+        
         server.start();
         try {
             server.bind(54123);
@@ -35,7 +42,10 @@ public class NSMServer {
             @Override
             public void received (Connection connection, Object object)
             {
-                
+                if(object instanceof LoginRequest)
+                {
+                    //todo: login
+                }
             }
         });
     }
