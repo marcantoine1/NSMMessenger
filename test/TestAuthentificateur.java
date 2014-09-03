d5c753c81c7120b37e67c3269d53526c315253bd
1150275 <1150275@10.159.17.114>
2014-09-03 09:00:56 -0400

    Ajouté les packages de test

---
+++ test/TestAuthentificateur.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 1150275
+ */
+public class TestAuthentificateur {
+    
+    public TestAuthentificateur() {
+    }
+    private DocumentBuilderFactory xmlFactory;
+    DocumentBuilder builder;
+    @BeforeClass
+    public static void setUpClass() {
+
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    private void testUserToXml(){
+        
+    } 
+    private void testPasswordToXml(){
+        
+    }
+    private void testUserFromXml(){
+        
+    }
+    private void testPasswordFromXml(){
+        
+    }
+}
