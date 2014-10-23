7b5ebad08e7399c7c9255cd6260798d894f519d4
Martin <enfantcool@gmail.com>
2014-10-22 22:09:35 -0400

    Interface PageLogin

--- src/ca/qc/bdeb/P56/NSMMessenger/Vue/PageLogin.java
+++ src/ca/qc/bdeb/P56/NSMMessenger/Vue/PageLogin.java
@@ -1,7 +1,20 @@
 package ca.qc.bdeb.P56.NSMMessenger.Vue;
 
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+
 /**
  * Created by Martin on 2014-10-21.
  */
 public class PageLogin {
+
+    private Stage primaryStage;
+
+    public PageLogin() {
+
+    }
+
+    public PageLogin(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
 }
