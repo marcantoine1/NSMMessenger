446f66dbef96a2857285f1decd3cf15af7ae2deb
guillaume009 <boudreauguillaume09@gmail.com>
2014-11-04 10:14:01 -0500

    Classe pour les tests en JavaFx

--- test/ca/qc/bdeb/p56/NSMMessenger/Vue/FXMLControllerProfileTest.java
+++ test/ca/qc/bdeb/p56/NSMMessenger/Vue/FXMLControllerProfileTest.java
@@ -6,18 +6,22 @@
 
 package ca.qc.bdeb.p56.NSMMessenger.Vue;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 
 /**
  *
  * @author Ordinatron
  */
-public class FXMLControllerProfileTest{
-    @BeforeClass
-    public static void setUpClass() {
+public class FXMLControllerProfileTest extends Application{
+    @Rule public JavaFXThreadingRule javafxRule = new JavaFXThreadingRule();
+    @BeforeClass    
+    public static void setUpClass() {        
     }
     
     @AfterClass
@@ -30,5 +34,21 @@ public class FXMLControllerProfileTest{
     
     @After
     public void tearDown() {
+    }
+     @BeforeClass
+    public static void initJFX() {
+        Thread t = new Thread("JavaFX Init Thread") {
+            @Override
+            public void run() {
+                Application.launch(FxGUI.class, new String[0]);
+            }
+        };
+        t.setDaemon(true);
+        t.start();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
     }
 }
