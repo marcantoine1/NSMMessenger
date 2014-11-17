25314b530b4b9ef43e02127b51da9e4f83c6260f
enfantcool <enfantcool@SleepingMonster.clubinfo.bdeb.qc.ca>
2014-11-17 14:18:14 -0500

    nom de bd pas bon

--- test/ca/qc/bdeb/p56/NSMMessengerServer/Application/TestAuthentificateur.java
+++ test/ca/qc/bdeb/p56/NSMMessengerServer/Application/TestAuthentificateur.java
@@ -5,7 +5,6 @@ package ca.qc.bdeb.p56.NSMMessengerServer.Application;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import ca.qc.bdeb.P56.NSMMessengerServer.Application.Authentificateur;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -21,6 +20,8 @@ import org.junit.Test;
  */
 public class TestAuthentificateur {
 
+    private final String cle = "859E381543769334";
+
     public TestAuthentificateur() {
     }
     static Authentificateur TestAuthentificateur;
@@ -28,10 +29,10 @@ public class TestAuthentificateur {
     @BeforeClass
     public static void setUpClass() {
         TestAuthentificateur = Authentificateur.getInstanceAuthentificateur();
-        TestAuthentificateur.setNomBd("dbTest.db");
-        TestAuthentificateur.creerUtilisateur("coolGuillaume", "sexyahri123", "guillaumesamurai@hotmail.ca",12,"nomFamille","prenom","homme","http://cdn.crunchify.com/wp-content/uploads/2012/10/java_url.jpg");
-        TestAuthentificateur.creerUtilisateur("coolRobert", "sexyahri123", "robertfroid@hotmail.ca",12,"nomFamille","prenom","homme","http://cdn.crunchify.com/wp-content/uploads/2012/10/java_url.jpg");
-        TestAuthentificateur.creerUtilisateur("coolAndré", "sexyahri123", "andresuper@hotmail.ca",12,"nomFamille","prenom","homme","http://cdn.crunchify.com/wp-content/uploads/2012/10/java_url.jpg");
+        TestAuthentificateur.setNomBd("dbTest");
+        TestAuthentificateur.creerUtilisateur("coolGuillaume", "sexyahri123", "guillaumesamurai@hotmail.ca", 12, "nomFamille", "prenom", "homme", "http://cdn.crunchify.com/wp-content/uploads/2012/10/java_url.jpg");
+        TestAuthentificateur.creerUtilisateur("coolRobert", "sexyahri123", "robertfroid@hotmail.ca", 12, "nomFamille", "prenom", "homme", "http://cdn.crunchify.com/wp-content/uploads/2012/10/java_url.jpg");
+        TestAuthentificateur.creerUtilisateur("coolAndré", "sexyahri123", "andresuper@hotmail.ca", 12, "nomFamille", "prenom", "homme", "http://cdn.crunchify.com/wp-content/uploads/2012/10/java_url.jpg");
     }
 
     @AfterClass
@@ -40,22 +41,26 @@ public class TestAuthentificateur {
 
     @Before
     public void setUp() {
+
     }
 
     @After
     public void tearDown() {
     }
-     @Test 
+//TODO: test utilisateur insere
+    @Test
     public void AuthentificateurEstToujoursLeMeme() {
         assertEquals(TestAuthentificateur, Authentificateur.getInstanceAuthentificateur());
     }
+
     @Test
     public void AuthentifierUnUtilisateur() {
         assertTrue(TestAuthentificateur.authentifierUtilisateur("coolGuillaume", "sexyahri123"));
-        
+
     }
+
     @Test
-    public void TesterTrouverUnUtilisateur(){
+    public void TesterTrouverUnUtilisateur() {
         assertTrue(TestAuthentificateur.utilisateurExiste("coolGuillaume"));
     }
 }
